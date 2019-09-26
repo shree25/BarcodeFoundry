2a3e6d520dc24548cde4e76a3fd6dc059680b18c
Athul AK <Athulnath.Ariyarathazhe.Kuni@ibm.com>
2019-09-26 16:58:53 +0200

    Added Moesif analytics plugin

--- src/main/java/com/generator/SpringBootWebApplication.java
+++ src/main/java/com/generator/SpringBootWebApplication.java
@@ -2,12 +2,20 @@ package com.generator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import javax.servlet.Filter;
+import com.moesif.servlet.MoesifFilter;
 
 @SpringBootApplication
 public class SpringBootWebApplication {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+
+	@Bean
+	public Filter moesifFilter() {
+		return new MoesifFilter("eyJhcHAiOiIxNTA6NTQ0IiwidmVyIjoiMi4wIiwib3JnIjoiNjkwOjE3NSIsImlhdCI6MTU2OTQ1NjAwMH0.jQcz9TrBuoLw6kb9gU3t7BOYvbQYM66LKqiY4lxtG2g");
 	}
 
 }
